3a9246bafe9d7ceec07dd312c41312a642624304
matheusgomessouza <matheusg_souza@outlook.com>
2021-10-21 19:42:16 -0300

    Frontend created

--- react_heat/src/App.tsx
+++ react_heat/src/App.tsx
@@ -1,45 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useContext } from 'react';
+import styles from './App.module.scss'
+import { LoginBox } from './components/LoginBox'
+import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './contexts/auth'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const { user } = useContext(AuthContext);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      { !!user ? <SendMessageForm /> : <LoginBox /> }
+    </main>
   )
 }
 
-export default App
--- react_heat/src/main.tsx
+++ react_heat/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { App } from './App'
+import { AuthProvider } from './contexts/auth'
+
+import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
